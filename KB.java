fbab8458fea25317f9b2a1b4429cf45ed185cf54
20103319 <101190963+20103319@users.noreply.github.com>
2022-07-05 18:31:41 +0800

    Add files via upload

---
+++ KB.java
@@ -0,0 +1,48 @@
+
+package uts;
+import java.util.Scanner;
+/**
+ *
+ * @author marhaini hidayati_20103319
+ */
+public class KB {
+    public String nama;
+    private int jml;
+    Scanner input = new Scanner(System.in);
+
+    public void dataPengunjung(){
+        System.out.println("==============Data Pengunjung Kebun Binatang================");
+        System.out.print("Masukkan Nama Pengunjuntg :");
+        nama = input.next();
+        System.out.print("Pengunjung Ke :");
+        jml = input.nextInt();
+
+    }
+
+    public void setnama(String Nama){
+        this.nama = Nama;
+
+    }
+
+    public void setjml(String Jumlah){
+        this.nama = Jumlah;
+    }
+
+    public String getnama(){
+        return this.nama;
+
+    }
+
+    public int getjml(){
+       return this.jml;
+    }
+
+
+    public void listPengunjung(){
+        System.out.println("===========Daftar Pengunjung Kebun Binatang==============");
+        System.out.println("Masukkan Nama Pengunjung :"+this.nama);
+        System.out.println("Pengunjung Ke :"+this.jml);
+
+    }
+    
+}
---
+++ UTS.java
@@ -0,0 +1,19 @@
+
+package uts;
+
+/**
+ *
+ * @author marhaini hidayati_20103319
+ */
+public class UTS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        KB list = new KB();
+       list.dataPengunjung();
+       list.listPengunjung();
+    }
+    
+}
